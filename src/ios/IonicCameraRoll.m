b4e9ff5595bee8cc7221869fd24bfc39951ae178
Max Lynch <max@drifty.com>
2013-09-26 12:05:33 -0500

    Thumbnail

--- src/ios/IonicCameraRoll.m
+++ src/ios/IonicCameraRoll.m
@@ -1,8 +1,4 @@
 /**
- * Camera Roll PhoneGap Plugin. 
- *
- * Reads photos from the iOS Camera Roll.
- *
  * Copyright 2013 Drifty Co.
  * http://drifty.com/
  *
@@ -21,8 +17,6 @@
     dispatch_once(&pred, ^{
       library = [[ALAssetsLibrary alloc] init];
     });
-
-    // TODO: Dealloc this later?
     return library;
   }
   
@@ -33,13 +27,16 @@
  */
 - (void)getPhotos:(CDVInvokedUrlCommand*)command
 {
+  NSString *targetWidth = [command argumentAtIndex:0];
+  NSString *targetHeight = [command argumentAtIndex:1];
+  
   
   // Grab the asset library
   ALAssetsLibrary *library = [IonicCameraRoll defaultAssetsLibrary];
   
   // Run a background job
   [self.commandDelegate runInBackground:^{
-    
+    __block int count = 0;
     // Enumerate all of the group saved photos, which is our Camera Roll on iOS
     [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
       
@@ -55,18 +52,40 @@
         
         // Enumarate this group of images
         
-        [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+        [group enumerateAssetsWithOptions:NSEnumerationReverse usingBlock:^(ALAsset *asset, NSUInteger index, BOOL *stop) {
           
-          NSDictionary *urls = [result valueForProperty:ALAssetPropertyURLs];
+          UIImage* thumbImage = [UIImage imageWithCGImage:[asset thumbnail]];
+    
+          if(thumbImage) {
+            NSData *data = UIImagePNGRepresentation(thumbImage);
+            
+            //[fileImage addObject:myImage];
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[data base64EncodedStringWithOptions:0]];
+            [pluginResult setKeepCallbackAsBool:YES];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            
+            NSLog(@"Count: %d", count);
+            count++;
+            if(count > 32) {
+              stop = YES;
+            }
+          }
+          
+          /*
+          NSDictionary *urls = [asset valueForProperty:ALAssetPropertyURLs];
           
           [urls enumerateKeysAndObjectsUsingBlock:^(id key, NSURL *obj, BOOL *stop) {
-
+            NSString* imageUrl = obj.absoluteString;
+            UIImage* image = nil;
+            
+            
             // Send the URL for this asset back to the JS callback
             CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:obj.absoluteString];
             [pluginResult setKeepCallbackAsBool:YES];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
           
           }];
+          */
         }];
       }
     } failureBlock:^(NSError *error) {
@@ -77,6 +96,59 @@
   }];
 
 }
+  
+  - (UIImage*)imageByScalingAndCroppingForSize:(UIImage*)anImage toSize:(CGSize)targetSize
+  {
+    UIImage* sourceImage = anImage;
+    UIImage* newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
+      CGFloat widthFactor = targetWidth / width;
+      CGFloat heightFactor = targetHeight / height;
+      
+      if (widthFactor > heightFactor) {
+        scaleFactor = widthFactor; // scale to fit height
+      } else {
+        scaleFactor = heightFactor; // scale to fit width
+      }
+      scaledWidth = width * scaleFactor;
+      scaledHeight = height * scaleFactor;
+      
+      // center the image
+      if (widthFactor > heightFactor) {
+        thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+      } else if (widthFactor < heightFactor) {
+        thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+      }
+    }
+    
+    UIGraphicsBeginImageContext(targetSize); // this will crop
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if (newImage == nil) {
+      NSLog(@"could not scale image");
+    }
+    
+    // pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    return newImage;
+  }
 
 @end
 
