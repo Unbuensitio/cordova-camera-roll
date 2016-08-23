8d40ea3086d14278e41b57719ad1a854b609eb5d
Florian Rival <Florian.Rival@gmail.com>
2016-08-23 14:25:14 +0200

    Add support for count options on iOS to limit the number of returned images

--- src/ios/IonicCameraRoll.m
+++ src/ios/IonicCameraRoll.m
@@ -1,5 +1,5 @@
 /**
- * Camera Roll PhoneGap Plugin. 
+ * Camera Roll PhoneGap Plugin.
  *
  * Reads photos from the iOS Camera Roll.
  *
@@ -15,29 +15,29 @@
 
 @implementation IonicCameraRoll
 
-  + (ALAssetsLibrary *)defaultAssetsLibrary {
++ (ALAssetsLibrary *)defaultAssetsLibrary {
     static dispatch_once_t pred = 0;
     static ALAssetsLibrary *library = nil;
     dispatch_once(&pred, ^{
-      library = [[ALAssetsLibrary alloc] init];
+        library = [[ALAssetsLibrary alloc] init];
     });
-
+    
     // TODO: Dealloc this later?
     return library;
-  }
-  
+}
+
 - (void)saveToCameraRoll:(CDVInvokedUrlCommand*)command
 {
-  NSString *base64String = [command argumentAtIndex:0];
-  NSURL *url = [NSURL URLWithString:base64String];    
-  NSData *imageData = [NSData dataWithContentsOfURL:url];
-  UIImage *image = [UIImage imageWithData:imageData];
+    NSString *base64String = [command argumentAtIndex:0];
+    NSURL *url = [NSURL URLWithString:base64String];
+    NSData *imageData = [NSData dataWithContentsOfURL:url];
+    UIImage *image = [UIImage imageWithData:imageData];
 
-  // save the image to photo album
-  UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    // save the image to photo album
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
 
-  CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"saved"];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"saved"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 /**
  * Get all the photos in the library.
@@ -46,51 +46,75 @@
  */
 - (void)getPhotos:(CDVInvokedUrlCommand*)command
 {
-  
-  // Grab the asset library
-  ALAssetsLibrary *library = [IonicCameraRoll defaultAssetsLibrary];
-  
-  // Run a background job
-  [self.commandDelegate runInBackground:^{
-    
-    // Enumerate all of the group saved photos, which is our Camera Roll on iOS
-    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
-      
-      // When there are no more images, the group will be nil
-      if(group == nil) {
-        
+    NSUInteger limit = 0;
+    if ([command.arguments count] > 0) {
+        NSString* limitStr = [command.arguments objectAtIndex:0];
+        limit = [limitStr integerValue];
+    }
+    bool hasLimit = limit > 0;
+
+    NSLog(@"getPhotos called with limit: %tu", limit);
+
+    // Grab the asset library
+    __block NSUInteger count = 0;
+    ALAssetsLibrary *library = [IonicCameraRoll defaultAssetsLibrary];
+
+    void (^signalEnumerationEnd)() = ^void() {
         // Send empty JSON to indicate the end of photostreaming
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{}];
         [pluginResult setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-      
-      } else {
+    };
 
-        // Enumarate this group of images
-        [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
-          
-          NSDictionary *urls = [result valueForProperty:ALAssetPropertyURLs];
-          NSDate* date = [result valueForProperty:ALAssetPropertyDate];
+    // Run a background job
+    [self.commandDelegate runInBackground:^{
 
-          [urls enumerateKeysAndObjectsUsingBlock:^(id key, NSURL *obj, BOOL *stop) {
-             //Only return JPG
-             if ([key isEqualToString:@"public.jpeg"]) {
-                 // Send the URL for this asset back to the JS callback
-                 CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"path": obj.absoluteString, @"date": [NSNumber numberWithLongLong:date.timeIntervalSince1970*1000]}];
-                 [pluginResult setKeepCallbackAsBool:YES];
-                 [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-             }
-          }];
+        // Enumerate all of the group saved photos, which is our Camera Roll on iOS
+        [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+
+            // When there are no more images, the group will be nil
+            if(group == nil || (hasLimit && count >= limit)) {
+                signalEnumerationEnd();
+                return;
+            }
+
+            // Enumarate this group of images
+            [group enumerateAssetsWithOptions:NSEnumerationReverse usingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+
+                if(hasLimit && count >= limit) {
+                    signalEnumerationEnd();
+                    return;
+                }
+
+                NSDictionary *urls = [result valueForProperty:ALAssetPropertyURLs];
+                NSDate* date = [result valueForProperty:ALAssetPropertyDate];
+
+                [urls enumerateKeysAndObjectsUsingBlock:^(id key, NSURL *obj, BOOL *stop) {
+
+                    if(hasLimit && count >= limit) {
+                        signalEnumerationEnd();
+                        return;
+                    }
+
+                    //Only return JPG
+                    if ([key isEqualToString:@"public.jpeg"]) {
+                        // Send the URL for this asset back to the JS callback
+                        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"path": obj.absoluteString, @"date": [NSNumber numberWithLongLong:date.timeIntervalSince1970*1000]}];
+                        [pluginResult setKeepCallbackAsBool:YES];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+                        count++;
+                    }
+                }];
+            }];
+
+        } failureBlock:^(NSError *error) {
+            // Ruh-roh, something bad happened.
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }];
-      }
-    } failureBlock:^(NSError *error) {
-      // Ruh-roh, something bad happened.
-      CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-      [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
-  }];
 
 }
 
 @end
-
--- www/CameraRoll.js
+++ www/CameraRoll.js
@@ -3,7 +3,8 @@ var exec = require('cordova/exec');
 var cameraRoll = {};
 
 cameraRoll.getPhotos = function(successCallback, errorCallback, options) {
-  exec(successCallback, errorCallback, "CameraRoll", "getPhotos", []);
+  const count = options ? options.count : 0;
+  exec(successCallback, errorCallback, "CameraRoll", "getPhotos", [count || 0]);
 };
 
 cameraRoll.saveToCameraRoll = function(imageBase64, successCallback, errorCallback, options) {
