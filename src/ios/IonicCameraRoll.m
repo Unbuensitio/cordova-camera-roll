3af7c35c28a98491dfef6c0767a06411df3acef4
Unbuensitio <info@unbuensitio.com>
2019-05-28 13:25:04 +0200

    Update IonicCameraRoll.m

--- src/ios/IonicCameraRoll.m
+++ src/ios/IonicCameraRoll.m
@@ -200,7 +200,7 @@
 {
     NSURL *url = [NSURL fileURLWithPath:filepath];
 
-    AVAsset *asset = [AVAsset assetWithURL:url];
+   /* AVAsset *asset = [AVAsset assetWithURL:url];
     AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
 self.imageGenerator.appliesPreferredTrackTransform = YES; 
         CMTime time = [asset duration];
@@ -211,7 +211,14 @@ self.imageGenerator.appliesPreferredTrackTransform = YES;
      CGImageRef imgRef = [self.imageGenerator copyCGImageAtTime:CMTimeMake(i, duration) actualTime:NULL error:nil];
      UIImage* thumbnail = [[UIImage alloc] initWithCGImage:imgRef scale:UIViewContentModeScaleAspectFit orientation:UIImageOrientationUp];
     [thumbnailImages addObject:thumbnail];
-    }
+    }*/
+    
+    AVURLAsset* asset = [AVURLAsset URLAssetWithURL:url options:nil];
+    AVAssetImageGenerator* generator = [AVAssetImageGenerator assetImageGeneratorWithAsset:asset];
+    generator.appliesPreferredTrackTransform = YES;
+    UIImage* image = [UIImage imageWithCGImage:[generator copyCGImageAtTime:CMTimeMake(0, 1) actualTime:nil error:nil]];
+    
+    return image;
 }
 
 @end
