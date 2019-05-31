ae07dc3b30ee260f6d922ad58383e0fb1bab6284
Unbuensitio <info@unbuensitio.com>
2019-05-31 20:21:36 +0200

    Update IonicCameraRoll.m

--- src/ios/IonicCameraRoll.m
+++ src/ios/IonicCameraRoll.m
@@ -123,7 +123,7 @@
                                         NSString *final = [imageData base64EncodedStringWithOptions:0];
                                         NSString* rutaImagen = [inicio stringByAppendingString:final];
                                         
-                                        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"path": ruta, @"date": [NSNumber numberWithLongLong:date.timeIntervalSince1970*1000]}];
+                                        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"path": rutaImagen, @"date": [NSNumber numberWithLongLong:date.timeIntervalSince1970*1000]}];
                                         [pluginResult setKeepCallbackAsBool:YES];
                                         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                                         count++;
