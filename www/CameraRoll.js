8f62a860e7c8ee0a39f7b0c48221396b366f5fd5
Unbuensitio <info@unbuensitio.com>
2019-06-01 17:40:21 +0200

    Update CameraRoll.js

--- www/CameraRoll.js
+++ www/CameraRoll.js
@@ -7,6 +7,11 @@ cameraRoll.getPhotos = function(successCallback, errorCallback, options) {
   exec(successCallback, errorCallback, "CameraRoll", "getPhotos", [count || 0]);
 };
 
+cameraRoll.obtenerLivePhotos = function(successCallback, errorCallback, options) {
+  const count = options ? options.count : 0;
+  exec(successCallback, errorCallback, "CameraRoll", "obtenerLivePhotos", [count || 0]);
+};
+
 cameraRoll.getVideos = function(successCallback, errorCallback, options) {
   const count = options ? options.count : 0;
   exec(successCallback, errorCallback, "CameraRoll", "getVideos", [count || 0]);
