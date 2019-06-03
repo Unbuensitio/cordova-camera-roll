d18cee966db5d5769e87844a05b97fc029002131
Unbuensitio <info@unbuensitio.com>
2019-06-03 08:05:37 +0200

    Update IonicCameraRoll.m

--- src/ios/IonicCameraRoll.m
+++ src/ios/IonicCameraRoll.m
@@ -108,7 +108,7 @@
                                        }
                                        else
                                        {
-                                                CIImage *image = [CIImage imageWithContentsOfURL:contentEditingInput.fullSizeImageURL];
+                                                CIImage *ciImage = [CIImage imageWithContentsOfURL:contentEditingInput.fullSizeImageURL];
                                                 UIImage* thumbnail = [UIImage imageWithCIImage:ciImage];
                                                 NSData *imageData = UIImageJPEGRepresentation(thumbnail, 100);
                                                 NSString *inicio = @"data:image/jpeg;base64,";
